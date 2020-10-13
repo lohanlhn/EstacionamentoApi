177309bae7623695ae0309eeb0ca8605c6aefa5c
lohanlhn <lohan.massote@gmail.com>
2020-10-13 20:23:55 -0300

    script SQL corrigido

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -12,6 +12,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `Usuario` (
   `id` INT NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
   `senha` VARCHAR(100) NOT NULL,
   `tipo` CHAR(1) NOT NULL,
   `dataAcesso` DATETIME NOT NULL,
@@ -25,7 +26,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Cliente` (
   `id` INT NOT NULL,
-  `telefone` VARCHAR(12) NULL,
+  `telefone` CHAR(11) NULL,
   `cpf` VARCHAR(12) NULL,
   `idUsuario` INT NOT NULL,
   PRIMARY KEY (`id`),
@@ -44,12 +45,12 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `Veiculo` (
   `id` INT NOT NULL,
   `marca` VARCHAR(100) NULL,
-  `modelo` VARCHAR(100) NULL,
   `cor` VARCHAR(100) NULL,
   `placa` CHAR(7) NOT NULL,
   `tipo` CHAR(1) NULL,
   `idCliente` INT NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE INDEX `placa_UNIQUE` (`placa` ASC) VISIBLE,
   INDEX `FK_Veiculo_Cliente_idx` (`idCliente` ASC) VISIBLE,
   CONSTRAINT `FK_Veiculo_Cliente`
     FOREIGN KEY (`idCliente`)
@@ -64,9 +65,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Vaga` (
   `id` INT NOT NULL,
-  `patio` VARCHAR(100) NOT NULL,
-  `disponivel` BIT NOT NULL,
   `codVaga` CHAR(3) NOT NULL,
+  `disponivel` BIT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -92,33 +92,6 @@ CREATE TABLE IF NOT EXISTS `Vaga_Ocupada` (
   CONSTRAINT `FK_Veiculo_Vaga_Veiculo`
     FOREIGN KEY (`idVeiculo`)
     REFERENCES `Veiculo` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-CREATE TABLE `Regra` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(50) NOT NULL,
-  `descricao` VARCHAR(255) NOT NULL,
-  `ativo` BIT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `nome_UNIQUE` (`nome` ASC))
-ENGINE = InnoDB;
- 
-CREATE TABLE `Usuario_Regra` (
-  `idUsuario` INT NOT NULL,
-  `idRegra` INT NOT NULL,
-  PRIMARY KEY (`idUsuario`, `idRegra`),
-  INDEX `fk_Usuario_Regra_regraIdx` (`idRegra` ASC),
-  INDEX `fk_Usuario_regraIdUsuariox` (`idUsuario` ASC),
-  CONSTRAINT `fk_Usuario_Regra_Usuario`
-    FOREIGN KEY (`idUsuario`) 
-    REFERENCES `Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Usuario_Regra_Regra`
-    FOREIGN KEY (`idRegra`)
-    REFERENCES `Regra` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
