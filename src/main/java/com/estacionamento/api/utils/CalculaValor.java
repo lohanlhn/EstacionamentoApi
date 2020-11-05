e2bc5874cbb10381fe14ab55edba01ce6b7583b7
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2020-11-05 16:51:36 -0300

    Test

--- src/main/java/com/estacionamento/api/utils/CalculaValor.java
+++ src/main/java/com/estacionamento/api/utils/CalculaValor.java
@@ -17,32 +17,18 @@ public class CalculaValor {
 	public static double CalculaValores (List<Valores> valores, Optional<VagaOcupada> vagaOcupada ) throws ParseException {
 		
 		List<CalculaValor> calcula = new ArrayList<CalculaValor>();
-		CalculaValor aux = new CalculaValor();
 		
 		long tempo1 = vagaOcupada.get().getHoraEntrada().getTime();
 		long tempo2 = vagaOcupada.get().getHoraSaida().getTime();
 		long diff = tempo2 - tempo1;
 		long diffMin = diff/60000;
-		long total = 0;
-		ArrayList<Long> totais = new ArrayList<Long>();
-		total =(diffMin % valores.get(0).getMinutagem());
 		
-		for(int i = 0; i < valores.size(); i++) {
-			totais.add(total);
-			total =(diffMin % valores.get(i).getMinutagem());
-			
-			
-			
-			if (total == 0) {
-				aux.valor = valores.get(i).getValor();
-				aux.tik = (int) (totais.get(i) / valores.get(i).getMinutagem());
-				calcula.add(aux);
-				
-				
-				break;
-			}
-			
-		}
+		
+		
+		
+		
+		
+		
 		vagaOcupada.get().setValor(0);
 		
 		for(int i = 0; i < calcula.size(); i++) {
