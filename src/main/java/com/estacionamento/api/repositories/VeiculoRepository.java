43d5b0987242d61688abb0369153fc639322c69f
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2020-11-09 10:16:54 -0300

    Teste se funciona

--- src/main/java/com/estacionamento/api/repositories/VeiculoRepository.java
+++ src/main/java/com/estacionamento/api/repositories/VeiculoRepository.java
@@ -2,9 +2,6 @@ package com.estacionamento.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
