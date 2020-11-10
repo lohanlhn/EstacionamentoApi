99b4cfe8409a8b03852158e0e058a248080102ec
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2020-11-10 17:57:22 -0300

    Arquivos VagaOcupada Repositoty

--- src/main/java/com/estacionamento/api/repositories/VagaOcupadaRepository.java
+++ src/main/java/com/estacionamento/api/repositories/VagaOcupadaRepository.java
@@ -1,10 +1,27 @@
 package com.estacionamento.api.repositories;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.estacionamento.api.entities.VagaOcupada;
 
 public interface VagaOcupadaRepository extends JpaRepository<VagaOcupada, Integer>{
+	
+	
+	@Transactional
+   	@Modifying(clearAutomatically = true)
+   	@Query("UPDATE VagaOcupada SET horaSaida = :novaHoraSaida WHERE id = :id")
+	void alterarHoraSaida(@Param("novaHoraSaida") Date novaHoraSaida, @Param("id") int id);
+	
+	@Transactional
+   	@Modifying(clearAutomatically = true)
+   	@Query("UPDATE VagaOcupada SET valor = :novoValor WHERE id = :id")
+	void alterarValor(@Param("novoValor") double valor, @Param("id") int id);
 	
 
 }
